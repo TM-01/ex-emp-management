c4d3dcf1428e7fd709676109158efdfc3c4d3621
TM-01 <tatsuro.miyazaki@rakus.co.jp>
2020-06-17 14:15:48 +0900

    insertの実装
    AdministratorRepositoryへの値を渡すためのService
    
    ControllerにInsertのスタートを記述

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,13 +1,20 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
-
+/**
+ * コントローラー.
+ * @author tatsuro.miyazaki
+ * 
+ */
 @Controller
 @RequestMapping("/")
 public class AdministratorController {
@@ -19,9 +26,30 @@ public class AdministratorController {
 		return new InsertAdministratorForm();
 	}
 	
+	/**
+	 * 
+	 * @return 入力フォーム
+	 * 入力フォームの表示
+	 */
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "administrator/insert.html";
+	}
+	
+	/**
+	 * 
+	 * @param form
+	 * formの内容をdomainにコピー
+	 * insertの実行
+	 * @return /
+	 * リダイレクトで最初に戻る
+	 */
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator administrator =new Administrator();
+		BeanUtils.copyProperties(form, administrator);
+		administratorService.insert(administrator);
+		return "redirect:/";
 	}
 
 }
